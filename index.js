0e66e741e988e310d0de91d123e36044b8ed240f
sayemaa <sayema990@gmail.com>
2022-05-08 13:41:27 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000;
+
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erud8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('warehouse').collection('item');
+
+        app.get('/inventory', async (req, res) => {
+            const query = {};
+            const cursor = itemCollection.find(query);
+            const items = await cursor.toArray();
+            res.send(items);
+        });
+
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await itemCollection.findOne(query);
+            res.send(item);
+        })
+
+        // Deliver & Restock items
+        app.put('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const restockItem = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const restockDoc = {
+                $set: {
+                    quantity: restockItem.quantity
+                }
+            };
+            const result = await itemCollection.updateOne(filter, restockDoc, options);
+            res.send(result);
+        })
+
+        // Add Item
+        app.post('/inventory', async (req, res) => {
+            const newItem = req.body;
+            const result = await itemCollection.insertOne(newItem);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Running Warehouse');
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+})
