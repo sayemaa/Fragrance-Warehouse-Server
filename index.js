8c794ac18013c5fbec2da0e18e6093ad599ef1bb
sayemaa <sayema990@gmail.com>
2022-05-08 23:19:54 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,22 @@ require('dotenv').config();
 // middleware
 app.use(cors());
 app.use(express.json());
+
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        console.log('decoded', decoded)
+        req.decoded = decoded;
+        next();
+    })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.erud8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -19,6 +36,14 @@ async function run() {
         await client.connect();
         const itemCollection = client.db('warehouse').collection('item');
 
+        /* AUTH */
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ accessToken });
+        })
+
+        /* INVENTORY API */
         app.get('/inventory', async (req, res) => {
             const query = {};
             const cursor = itemCollection.find(query);
@@ -49,6 +74,20 @@ async function run() {
         })
 
         // Add Item (POST)
+        app.get('/myitems', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
+            const email = req.query.email;
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = itemCollection.find(query);
+                const myItems = await cursor.toArray();
+                res.send(myItems);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' });
+            }
+        })
+
         app.post('/inventory', async (req, res) => {
             const newItem = req.body;
             const result = await itemCollection.insertOne(newItem);
